a1cbff3ee2afab039acd92d6e8cdb887362ebec8
anshuljharbade <jharbade.ashish@gmail.com>
2020-10-20 22:34:52 +0530

    Add build folder to public folder
    Signed-off-by: anshuljharbade <jharbade.ashish@gmail.com>

---
+++ sls-course-frontend-master/public/build/precache-manifest.dee380f32cf6e859c0b917a6049e8bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f669aa38b1e49ca28c89fecfd38eab56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f074723ad70ff7489b08",
+    "url": "/static/css/main.854a357d.chunk.css"
+  },
+  {
+    "revision": "6533c94665e6c7fa5231",
+    "url": "/static/js/2.4c37cf12.chunk.js"
+  },
+  {
+    "revision": "5727441246ffa49345a3574f984c2034",
+    "url": "/static/js/2.4c37cf12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f074723ad70ff7489b08",
+    "url": "/static/js/main.71781154.chunk.js"
+  },
+  {
+    "revision": "50d81e894c4c564ba66d",
+    "url": "/static/js/runtime-main.e0edc982.js"
+  }
+]);
